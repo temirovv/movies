7ba1995dd58d01a175a2c9b3ce7569e55ebf9e74
temirovv <temirovv21@gmail.com>
2024-11-19 18:36:58 +0500

    Static and Media files configured

--- root/urls.py
+++ root/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +10,7 @@ urlpatterns = [
     
     path('', include('movies.urls'))
 ]
+
+if settings.DEBUG == True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
