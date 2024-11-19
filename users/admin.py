d50faa101a3b052bba0a56c6b54346d49867c339
temirovv <temirovv21@gmail.com>
2024-11-19 17:35:19 +0500

    user model configured

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.admin import register
+from .models import User
 
-# Register your models here.
+
+@register(User)
+class CustomUserAdmin(UserAdmin):
+    pass
