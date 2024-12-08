fb88dcea406bfcdfb5a1d345ff7bea6d523f65bc
temirovv <temirovv21@gmail.com>
2024-12-08 11:24:31 +0500

    changed movie_detail_slug

--- movies/context_processors.py
+++ movies/context_processors.py
@@ -1,6 +1,6 @@
 from .models import Movie
 
 def movie_detail_slug(request):
-    movie_detail_slug = Movie.objects.first().slug
-
-    return {'movie_detail_slug': movie_detail_slug}
+    if Movie.objects.first():
+        movie_detail_slug = Movie.objects.first().slug
+        return {'movie_detail_slug': movie_detail_slug}
