68ec7a28394c8e86d8e09722ada9921b9e294087
temirovv <temirovv21@gmail.com>
2024-12-19 17:22:55 +0500

    Settings fixed

--- root/settings/base.py
+++ root/settings/base.py
@@ -4,6 +4,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 sys.path.append(str(BASE_DIR / 'root/settings'))
+sys.path.append(str(BASE_DIR / 'root'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
--- root/settings/production.py
+++ root/settings/production.py
@@ -3,3 +3,7 @@ from root.settings.base import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['188.225.34.204', 'http://188.225.34.204', 'http://bahromtravel.uz/', 'www.bahromtravel.uz', 'bahromtravel.uz']
+print('ishladi')
+
+ROOT_URLCONF = 'root.urls'
+
